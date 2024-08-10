d7fea13852640a88ccf037b48dab821036073616
Prabesh789 <61115432+Prabesh789@users.noreply.github.com>
2024-08-10 00:25:05 -0400

    Delete where_clause_&_like_(wildcards)_clause.sql
    File deleted

--- where_clause_&_like_(wildcards)_clause.sql
+++
@@ -1,5 +0,0 @@
-```sql
-/* LIKE: A clause used for pattern matching in SQL.
-Wildcards: Special characters (%, _) used within the LIKE clause to define patterns.
-Relationship: LIKE uses wildcards to perform flexible searches, but LIKE itself is not a wildcard.
-*/
